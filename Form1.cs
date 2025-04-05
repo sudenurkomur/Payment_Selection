fca89f273676b81c6bcc669acbc9481337844be8
Sude Nur Kömür <sudenurkomur1253@gmail.com>
2025-04-05 20:16:36 +0300

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,38 @@
+namespace Payment_Selection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Payments/IPayment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payment_Selection.Payments
+{
+    internal interface IPayment
+    {
+        string Pay(int amount);
+    }
+}
